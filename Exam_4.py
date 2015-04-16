2e615ded8efe6bc0a53daab7d9e6f79aee0f2efe
chivaachino <art_morelia@hotmail.com>
2015-04-16 12:06:35 -0500

    Update Exam_4.py

--- Exam_4.py
+++ Exam_4.py
@@ -1,1 +1,14 @@
-
+def fib(y):
+    x=0
+    z=0
+    f=0
+    while(x<y):
+        x=x+1
+        z=z+x
+    
+    return z
+    
+     
+y = int(input("Dame un numero entero positivo para obtener la serie fibonacci: "))
+res = fib(y)
+print(res)
