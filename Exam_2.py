18335def4a5e69ee8195e1f8bf4699bf04602eef
chivaachino <art_morelia@hotmail.com>
2015-04-16 12:07:33 -0500

    Update Exam_2.py

--- Exam_2.py
+++ Exam_2.py
@@ -1,1 +1,10 @@
+def triangulo(SIZE):
+    for n in range(1,SIZE+1):
+        print("T"*n)
+    for n in range(1,SIZE):
+        s = SIZE-n
+        print("T"*s)
 
+
+SIZE = int(input("Dime el tamaño del triangulo: "))        
+triangulo(SIZE)        
