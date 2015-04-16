3239d4fe0e8d88449aba41336f7a35b2ba04cc7e
chivaachino <art_morelia@hotmail.com>
2015-04-16 12:11:23 -0500

    Update Exam_3.py

--- Exam_3.py
+++ Exam_3.py
@@ -1,1 +1,15 @@
+def superpower(num1,num2):
+    res = 1
+    pot = 1
+	while(pot<num2+1):
+		res = res * num1
+		pot = pot +1 
+		
+
+    
+num1 = int(input("Dame el numero que deseas elevar a una potencia: "))
+num2 = int(input("¿A cual potencia lo quieres elevar? "))
+final = superpower(num1,num2)
+print("El resultado es: "+str(final))
+
 
